394de11297e6bf09f4ff9e4982f54e72814140d6
darrus <darrusgoh@gmail.com>
2019-06-30 23:11:06 +0800

    Completed section 5

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Person from "./Person/Person";
 import Radium, { StyleRoot } from "radium";
 
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   state = {
@@ -36,19 +36,9 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    }
-
     let persons = null;
+    let btnClass = null;
+
     if(this.state.showPersons) {
       persons = (
         <div>
@@ -63,30 +53,26 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor:'salmon',
-        color:'black'
-      };
+      btnClass = classes.Red;
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if(this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push('red');
     }
 
     if(this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push('bold');
     }
 
-    classes = classes.join(' ');
+    assignedClasses = assignedClasses.join(' ');
 
     return (
       <StyleRoot>
-        <div className="App">
-          <p className={classes}>Hello world!</p>
-          <button 
-          style={style}
+        <div className={classes.App}>
+          <p className={assignedClasses}>Hello world!</p>
+          <button
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import Radium from 'radium';
 
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
-
     return (
-    <div className="Person" style={style}>
+    <div className={classes.Person}>
         <p onClick={props.click}>Hello my name is {props.name}. I am {props.age} years old.</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
